17ab93a4eb4058da1924874cfcc7333ec3b5da14
Staś Małolepszy <stas@duzodobrze.pl>
2019-10-13 00:54:55 +0200

    Make balls explode

--- src/systems/sys_control_ball.ts
+++ src/systems/sys_control_ball.ts
@@ -1,7 +1,9 @@
+import {grow} from "../components/com_grow.js";
 import {Get} from "../components/com_index.js";
+import {lifespan} from "../components/com_lifespan.js";
 import {Entity, Game} from "../game.js";
 
-const QUERY = Get.Transform | Get.ControlBall | Get.Move;
+const QUERY = Get.Transform | Get.ControlBall | Get.Move | Get.Collide;
 
 export function sys_control_ball(game: Game, delta: number) {
     for (let i = 0; i < game.World.length; i++) {
@@ -21,5 +23,15 @@ function update(game: Game, entity: Entity, delta: number) {
 
     if (transform.Translation[1] < 0 || transform.Translation[1] > game.Canvas.height) {
         move.Direction[1] = -move.Direction[1];
+    }
+
+    let collide = game[Get.Collide][entity];
+    for (let i = 0; i < collide.Collisions.length; i++) {
+        let other = collide.Collisions[i].EntityId;
+        if (game.World[other] & Get.Grow) {
+            game.World[entity] &= ~Get.Move;
+            grow(40)(game, entity);
+            lifespan(3)(game, entity);
+        }
     }
 }
