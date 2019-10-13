900ea998f7f1aa54596ec8a918c63a4750d49dea
Staś Małolepszy <stas@duzodobrze.pl>
2019-10-13 11:35:45 +0200

    Parameterize ball count

--- src/worlds/wor_stage.ts
+++ src/worlds/wor_stage.ts
@@ -2,10 +2,10 @@ import {create_ball} from "../blueprints/blu_ball.js";
 import {Game} from "../game.js";
 import {integer} from "../math/random.js";
 
-export function world_stage(game: Game) {
+export function world_stage(game: Game, ball_count: number) {
     game.World = [];
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < ball_count; i++) {
         game.Add(
             create_ball(game, integer(1, game.Canvas.width - 1), integer(1, game.Canvas.height - 1))
         );
