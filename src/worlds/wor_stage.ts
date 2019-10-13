6675d443e64ff4582c250765c0ded1323308f0df
Staś Małolepszy <stas@duzodobrze.pl>
2019-10-13 10:08:41 +0200

    50 balls

--- src/worlds/wor_stage.ts
+++ src/worlds/wor_stage.ts
@@ -5,7 +5,7 @@ import {integer} from "../math/random.js";
 export function world_stage(game: Game) {
     game.World = [];
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 50; i++) {
         game.Add(
             create_ball(game, integer(1, game.Canvas.width - 1), integer(1, game.Canvas.height - 1))
         );
