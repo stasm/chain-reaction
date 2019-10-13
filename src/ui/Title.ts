d5544a40ab6a181ee1e80e33ea3003fac724102d
Staś Małolepszy <stas@duzodobrze.pl>
2019-10-13 17:59:17 +0200

    (ui/Title) Typography tweaks

--- src/ui/Title.ts
+++ src/ui/Title.ts
@@ -3,16 +3,19 @@ import {html} from "./html.js";
 
 export function Title() {
     return html`
-        <div style="font: 20vmin/1.2 sans-serif;">
-            <div style="margin-top: 10vh">
-                <span>Chain</span><br />
-                <span>Reaction</span>
-            </div>
-            <div style="margin-top: 10vh">
-                <button onclick="$(${Action.GoToPlay}, [10])">10</button><br />
-                <button onclick="$(${Action.GoToPlay}, [100])">100</button><br />
-                <button onclick="$(${Action.GoToPlay}, [1000])">1000</button><br />
-            </div>
+        <div style="margin-top: 10vh; font: 20vmin sans-serif;">
+            <span>Chain</span><br><span>Reaction</span>
+        </div>
+        <div style="margin-top: 10vh; font: 10vmin sans-serif;">
+            <button onclick="$(${Action.GoToPlay}, [10])">
+                <span>Play Now</span>
+            </button>
+            <button onclick="$(${Action.GoToPlay}, [100])">
+                <span>One Hundred<span>
+            </button>
+            <button onclick="$(${Action.GoToPlay}, [1000])">
+                <span>One Thousand</span>
+            </button>
         </div>
     `;
 }
